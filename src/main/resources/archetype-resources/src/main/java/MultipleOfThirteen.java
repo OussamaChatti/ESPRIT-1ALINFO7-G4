1f7338b1d042417ca098156dea55a6c5928b3234
Seifallah Jami <seifallah.jami@avaxia-group.com>
2023-02-15 11:23:27 +0100

    Add multiple of 13 class

---
+++ src/main/resources/archetype-resources/src/main/java/MultipleOfThirteen.java
@@ -0,0 +1,11 @@
+package $tn.esprit.alternance;
+
+public class MultipleOfThirteen {
+    public static boolean isMultipleOfThirteen(int num) {
+        if (num % 13 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
