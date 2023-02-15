b9505fb99fce2ba8da6e93975450fc0647742be9
Louay Mansour <louay.mansour@avaxia-group.com>
2023-02-15 12:46:59 +0100

    modified by Louay

---
+++ src/main/resources/archetype-resources/src/main/java/MultipleOfFour.java
@@ -0,0 +1,10 @@
+package $tn.esprit.alternance;
+
+public class MultipleOfFour {
+
+    public static void isMultipleOfFour(int n) {
+        if (n % 4 == 0){
+            system.out.println("This number is multiple of Four!");
+        };
+    }
+}
